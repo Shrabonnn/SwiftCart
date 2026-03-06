b28a772a209f7cf28b43b3360774ffda02d6942c
shrabonnn <shrabonewmc009@gmail.com>
2026-03-06 23:57:06 +0600

    cart_list_screen ui changes

--- lib/view/cart/cart_list_screen.dart
+++ lib/view/cart/cart_list_screen.dart
@@ -23,13 +23,13 @@ class _CartListScreenState extends State<CartListScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        leading: IconButton(onPressed: (){
-          Get.off(HomeScreen());
-        }, icon: Icon(Icons.arrow_back)),
-        title: Text('My Cart',style: TextStyle(
-          fontWeight: FontWeight.bold
+        leading: IconButton(
+          onPressed: () {
+            Get.off(HomeScreen());
+          },
+          icon: Icon(Icons.arrow_back),
         ),
-        ),
+        title: Text('My Cart', style: TextStyle(fontWeight: FontWeight.bold)),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -37,31 +37,30 @@ class _CartListScreenState extends State<CartListScreen> {
           children: [
             Expanded(
               child: Column(
-                children: [
-                  _buildCartListItem(),
-                  _buildCartListItem(),
-                ],
+                children: [_buildCartListItem(), _buildCartListItem()],
               ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('Total',style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 18
-                  ),),
-                  Text('\$18000',style: TextStyle(
-                  fontWeight: FontWeight.w500,
+                  Text(
+                    'Total',
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+                  ),
+                  Text(
+                    '\$18000',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
                       fontSize: 18,
-                    color: Colors.blue.shade700
-
-                  ),),
-
+                      color: Colors.blue.shade700,
+                    ),
+                  ),
                 ],
               ),
             ),
-            SizedBox(height: 16,),
+            SizedBox(height: 16),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: CustomButton(title: "Buy Now", onTap: () {}),
@@ -74,63 +73,76 @@ class _CartListScreenState extends State<CartListScreen> {
 
   Row _buildCartListItem() {
     return Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 8),
-                      height: 120,
-                      width: 375,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade200,
-                        borderRadius: BorderRadius.circular(12),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.blueAccent,
-                            spreadRadius: 0.5,
-                            blurRadius: 2,
-                            offset: Offset(3,2)
-                          )
-                        ]
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 8),
+          height: 120,
+          width: 375,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.blueAccent,
+                spreadRadius: 0.5,
+                blurRadius: 2,
+                offset: Offset(3, 2),
+              ),
+            ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: AppColors.fieldBackground,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Image.asset(ImagePath.product),
+                ),
+                SizedBox(width: 8),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Apple W-series 6",
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                color: AppColors.fieldBackground,
-                                borderRadius: BorderRadius.circular(8)
-                              ),
-                              child: Image.asset(ImagePath.product),
-                            ),
-                            SizedBox(width: 8,),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text("Apple W-series 6",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),overflow: TextOverflow.ellipsis,maxLines: 1,),
-                                  SizedBox(height: 8,),
-                                  Text("\$300",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 17),),
-                                ],
-                              ),
-                            ),
-                            SizedBox(width: 10,),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text("Size: 35",style: TextStyle(color: Colors.black54)),
-                                SizedBox(height: 6),
-                                ProductQuantityIncDecButton(onChange: (int p1) {  },),
-                              ],
-                            )
-
-                          ],
+                      SizedBox(height: 8),
+                      Text(
+                        "\$300",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 17,
                         ),
                       ),
-                    )
+                    ],
+                  ),
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Size: 35", style: TextStyle(color: Colors.black54)),
+                    SizedBox(height: 6),
+                    ProductQuantityIncDecButton(onChange: (int p1) {}),
                   ],
-                );
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
